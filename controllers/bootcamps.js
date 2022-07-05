00d6d6ee6dfc594dcf4d3f0f643f045d47f69bad
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-05 07:41:49 +0530

    Add middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,45 +1,46 @@
 // @desc Get All bootcamps
-// @route GET /api/v1/bootcaps
+// @route GET /api/v1/bootcamps
 // @access Public
 
 exports.getBootcamps = (req, res, next) => {
-  res.status(400).json({ sucess: true, dsg: "Show all bootcamps" });
+  res.status(200).json({ sucess: true, dsg: "Show all bootcamps" });
 };
 
 // @desc Get a bootcamp
-// @route GET /api/v1/bootcaps/:id
+// @route GET /api/v1/bootcamps/:id
 // @access Public
 
 exports.getBootcamp = (req, res, next) => {
-  res
-    .status(400)
-    .json({ sucess: true, dsg: `Get  bootcamp id ${req.params.id}` });
+  res.status(200).json({
+    sucess: true,
+    dsg: `Get  bootcamp id ${req.params.id}`,
+  });
 };
 
 // @desc Create a bootcamp
-// @route POST /api/v1/bootcaps/:id
+// @route POST /api/v1/bootcamps/:id
 // @access Private
 
 exports.createBootcamp = (req, res, next) => {
-  res.status(400).json({ sucess: true, dsg: "Create all bootcamps" });
+  res.status(200).json({ sucess: true, dsg: "Create all bootcamps" });
 };
 
 // @desc Update a bootcamp
-// @route PUT /api/v1/bootcaps/:id
+// @route PUT /api/v1/bootcamps/:id
 // @access Private
 
 exports.updateBootcamp = (req, res, next) => {
   res
-    .status(400)
+    .status(200)
     .json({ sucess: true, dsg: `Update bootcamp id ${req.params.id}` });
 };
 
 // @desc Deletebootcamp
-// @route DELETE /api/v1/bootcaps/:id
+// @route DELETE /api/v1/bootcamps/:id
 // @access Private
 
 exports.deleteBootcamp = (req, res, next) => {
   res
-    .status(400)
+    .status(200)
     .json({ sucess: true, dsg: `Delete bootcamp id ${req.params.id}` });
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 // Route Files
 const bootcamps = require("./routes/bootcamps");
@@ -8,6 +9,11 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+//Dev Login middleware
+if (process.env.NODE_ENV === "develepment") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
