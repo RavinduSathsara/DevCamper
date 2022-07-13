7be9af9e55a6bbb3075d52605ec5c7ae2cac477e
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-13 09:32:12 +0530

    Add asnc middleware

--- middleware/error.js
+++ middleware/error.js
@@ -5,13 +5,26 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console
-  console.log(err.stack.red);
+  console.log(err);
 
   //   Mongoose bad ObjectID
   if (err.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate filed value entered.";
+    error = new ErrorResponse(message, 400);
+  }
+
+  //  Mongoose validation error
+  if (err.name === "validatorError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || "Server Error",
